4b57c06ed12adf8ebc8a983abaf1cd3723085282
dydhslxl7 <dydhslxl7@naver.com>
2022-01-20 22:39:53 +0900

    A_01_18 ing

--- baekjoon/A_01_18.java
+++ baekjoon/A_01_18.java
@@ -1,20 +1,26 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class A_01_18 {
-    public static void way1(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    // ing
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
 
-        int n = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int[] num = new int[n];
 
         for(int i=0; i<n; i++) {
-            num[i] = sc.nextInt();
+            num[i] = Integer.parseInt(st.nextToken());
         }
 
-        sc.close();
         Arrays.sort(num);
         System.out.println(num[0]+" "+num[4]);
     }
