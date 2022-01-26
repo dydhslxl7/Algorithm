0b0f6cafb38a9ab9899cc5dbd226683ba292f4d6
dydhslxl7 <dydhslxl7@naver.com>
2022-01-26 22:19:08 +0900

    A_01_26 > Q_2908

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -1,5 +1,6 @@
 package baekjoon;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.StringTokenizer;
@@ -40,5 +41,25 @@ public class Main {
         System.out.println(st.countTokens());
         
         in.close();
+    }
+
+    public static void Q_2908(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        StringTokenizer st = new StringTokenizer(str);
+        
+        int n2 = st.countTokens();
+        int[] num = new int[n2];
+        int n = 0;
+        for(int i=0; i<n2; i++) {
+            n = Integer.parseInt(st.nextToken());
+            num[i] += n / 100;
+            num[i] += n % 100 / 10 * 10;
+            num[i] += n % 10 * 100;
+        }
+        
+        Arrays.sort(num);
+
+        System.out.println(num[n2-1]);
     }
 }
