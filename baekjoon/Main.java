f8dd4ed0df8f8a7d8a1ed2748f1bb0083aa47872
dydhslxl7 <dydhslxl7@naver.com>
2022-02-01 20:32:32 +0900

    22-02-01

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -1,7 +1,14 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Main {
+    //10926
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(in.nextLine()+"??!");
+
+        in.close();
     }
 }
