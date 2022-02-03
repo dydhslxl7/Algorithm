30ddd83ebc109825fc8a926e45c45baa8c17bf3e
dydhslxl7 <dydhslxl7@naver.com>
2022-02-04 08:31:00 +0900

    A_02_04 2750

---
+++ baekjoon/A_02_04.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class A_02_04 {
+    //2750
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = Integer.parseInt(in.nextLine());
+
+        int[] num = new int[n];
+        for(int i=0; i<n; i++) {
+            num[i] = Integer.parseInt(in.nextLine());
+        }
+
+        Arrays.asort(num);
+
+        foreach(int n1 : num) {
+            System.out.println(n1);
+        }
+
+        in.close();
+    }
+}
