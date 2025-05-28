f0d17aed791c685657fd23921d47b8ae5e3d33e6
dydhslxl7 <dydhslxl7@naver.com>
2025-05-28 23:24:57 +0900

    enumerate 문제 추가 및 range, eval 함수 설명 추가

--- programmers/python/practice.py
+++ programmers/python/practice.py
@@ -1,10 +1,10 @@
-# 홀짝에 따라 다른 값 반환하기 -> range + sum
+# 홀짝에 따라 다른 값 반환하기 -> range + sum : range 함수 사용법 확인
 def solution(n):
     if n%2:
         return sum(range(1,n+1,2))
     return sum([i*i for i in range(2,n+1,2)])
 
-# 조건 문자열 -> eval
+# 조건 문자열 -> eval : 문자열을 수식으로 변환하여 실행하는 함수
 def solution(ineq, eq, n, m):
     if eq == "=" and n == m: return 1
 
@@ -20,3 +20,10 @@ def solution(ineq, eq, n, m):
     if eq == '!':
         eq = ''
     return int(eval(f'{n} {ineq}{eq} {m}'))
+
+# 등차수열의 특정한 항만 더하기 -> enumerate : 반복 가능한 객체를 순회할 때 (인덱스, 값) 형태로 반환
+def solution(a, d, included):
+    answer = 0
+    for index, flag in enumerate(included):
+        if flag: answer += a + (d * index)
+    return answer
