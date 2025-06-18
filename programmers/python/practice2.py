91ca011088ef0f8deb876a3613760c09580f674f
dydhslxl7 <dydhslxl7@naver.com>
2025-06-18 18:00:41 +0900

    비트 연산자

--- programmers/python/practice2.py
+++ programmers/python/practice2.py
@@ -32,3 +32,10 @@ def solution(arr, queries):
     for a,b in queries:
         arr[a],arr[b]=arr[b],arr[a] # 변수 언패킹 사용법 숙지
     return arr
+
+# 간단한 논리 연산
+def solution(x1, x2, x3, x4):
+    return ((x1 or x2) and (x3 or x4))
+
+def solution(x1, x2, x3, x4):
+    return ((x1 | x2) & (x3 | x4)) # 비트 연산자
