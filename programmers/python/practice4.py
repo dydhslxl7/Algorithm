9eea36f47bcd1c22cad4748a5e9af6c53224f118
dydhslxl7 <dydhslxl7@naver.com>
2025-06-25 12:35:38 +0900

    del 함수, join, for+list comprehension

--- programmers/python/practice4.py
+++ programmers/python/practice4.py
@@ -5,3 +5,15 @@ range와 list comprehension을 사용하여
 '''
 def solution(n, k):
     return [i for i in range(k, n+1, k)]
+
+# 글자 지우기 -> del 
+def solution(my_string, indices):
+    m = list(my_string)  # 문자열을 리스트로 변환
+    m = [m[i] for i in range(len(m)) if i not in indices]  # 인덱스에 해당하는 문자 제거
+    return ''.join(m)
+
+def solution(my_string, indices):
+    my_string = list(my_string)
+    for i in sorted(indices, reverse=True):
+        del my_string[i] 
+    return ''.join(my_string)
