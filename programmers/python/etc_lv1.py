5ea3c68a49e1bc5d3c49b8ed9c9e5ff4e576d1db
dydhslxl7 <dydhslxl7@naver.com>
2025-07-09 15:12:00 +0900

    그 외 3문제 풀이

--- programmers/python/etc_lv1.py
+++ programmers/python/etc_lv1.py
@@ -5,3 +5,26 @@ def solution(t, p):
         if int(t[i:i+len(p)]) <= int(p):
             answer += 1
     return answer
+
+# 없는 숫자 더하기
+def solution(numbers):
+    answer = 0
+    for i in range(10):
+        if i not in numbers:
+            answer += i
+    return answer
+
+# 부족한 금액 계산하기
+def solution(price, money, count):
+    answer = 0
+    for i in range(count+1):
+        answer += price * i
+    return 0 if money - answer > 0 else abs(money - answer)
+
+# 숫자 문자열과 영단어
+def solution(s):
+    num_dic = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+    for i in num_dic:
+        if i in s:
+            s = s.replace(i, str(num_dic[i]))
+    return int(s)
